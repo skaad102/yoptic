9bb766945ad232403ea872ce60f21cf79cd6c33d
John Doedaniel <skaad102@gmail.com>
2021-08-31 20:21:33 -0500

    slider de nosotro

---
+++ resources/js/slider.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.blog-slider', {
+  autoplay: {
+    delay: 4500,
+  },
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
