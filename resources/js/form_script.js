ec899bf7cf2002e4d3175dc81fb7db38c144b541
John Doedaniel <skaad102@gmail.com>
2021-09-02 09:01:36 -0500

    publicacion

--- resources/js/form_script.js
+++ resources/js/form_script.js
@@ -1,7 +1,17 @@
 "use strict";
 (function ($) {
   $.fn.initForm = function (options) {
-
+    let btnSend = $('#submit-message');
+    var settings = $.extend(
+      {
+        successClean: this.find(".form-success-clean"),
+        successGone: this.find(".form-success-gone"),
+        successInvisible: this.find(".form-success-invisible"),
+        successVisible: this.find(".form-success-visible"),
+        textFeedback: this.find(".form-text-feedback"),
+      },
+      options
+    );
     if (jQuery.validator) {
       jQuery.validator.setDefaults({ success: "valid" });
       this.validate({ rules: { field: { required: true, email: true } } });
@@ -12,7 +22,6 @@
         if ($(this).valid()) {
           const $form = document.querySelector("#message_form");
           $form.addEventListener("submit", handleSubmit);
-
           async function handleSubmit(e) {
             const form = new FormData(e.target);
             const response = await fetch(this.action, {
@@ -22,8 +31,9 @@
                 Accept: "application/json",
               },
             });
-
             if (response.ok) {
+              btnSend.toggleClass('clicked');
+              btnSend.prop('disabled', true);
               settings.successClean.val("");
               settings.successInvisible.addClass("invisible");
               settings.successGone.addClass("gone");
